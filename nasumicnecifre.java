b29b40024a9f9c5d092e0cf0fe79a4607f93419d
bttalic <b.ttalic@gmail.com>
2014-11-16 20:14:25 +0100

    Komentari

--- nasumicnecifre.java
+++ nasumicnecifre.java
@@ -2,6 +2,9 @@
 public class nasumicnecifre {
 	public static void main(String args[]){
 		double randombroj=10000+Math.random()*90000;
+		/*randombroj ce i nakon naredne linije koda ostati double, moras "spasiti" casting.
+		 * i sjeti se camel case - randomBroj
+		*/
 		System.out.println("Nasumicni broj je: " + (int) randombroj);
 		int cifra;
 		System.out.println("Cifre nasiumicnog broja: ");
